b72681433538a5eca85472627b64433b9b7cba2b
jackieryan <ruanxuewu@gmail.com>
2014-03-30 17:38:55 +0800

    first test

---
+++ CsdnDemo.cpp
@@ -0,0 +1,18 @@
+// CsdnDemo.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "hello world" << endl;
+	
+	system("pause");
+	return 0;
+}
